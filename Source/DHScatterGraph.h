56e734d78f593b0da4fce749ead8c841e3453ba8
Douglas Hill <hello@douglashill.co>
2015-05-14 11:16:38 +0100

    Add framework version info to umbrella header
    The values of these constants are provided by Xcode’s automatically generated DerivedSources/DHScatterGraph_vers.c.

--- Source/DHScatterGraph.h
+++ Source/DHScatterGraph.h
@@ -1,6 +1,9 @@
 //  Douglas Hill, May 2015
 //  https://github.com/douglashill/DHScatterGraph
 
+extern double const DHScatterGraphVersionNumber;
+extern unsigned char const DHScatterGraphVersionString[];
+
 #import <DHScatterGraph/DHScatterGraphLineAttributes.h>
 #import <DHScatterGraph/DHScatterGraphPointSet.h>
 #import <DHScatterGraph/DHScatterGraphView.h>
